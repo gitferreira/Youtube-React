b14e54e4cb3a7c53ace5e5ba3a8413d7235e7b46
gitferreira <iniferre96@gmail.com>
2023-02-21 15:05:41 +0100

    first commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,20 @@
+import {useState, useEffect} from 'react'
+import {Box, Stack, Typography} from "@mui/material"
+import { borderRight } from '@mui/system'
+import {SideBar} from './index'
+
+const Feed = () => {
+  return (
+    <Stack sx={{flexDirection: {sx: "column", md: "row" }}}>
+      <Box sx={{height: { sx: 'auto', md: "92vh", }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 }}}>
+    <SideBar />
+
+<Typography className='copyright' variant='body2' sx={{mt: 1.5, color: '#fff'}}>
+  COPYRIGHT 2023 IFR
+</Typography>
+      </Box>
+    </Stack>
+  )
+}
+
+export default Feed
---
+++ src/components/index.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import VideoDetail from "./VideoDetail";
+import ChannelDetail from "./ChannelDetail";
+import SearchFeed from "./SearchFeed";
+import SearchBar from "./SearchBar";
+import SideBar from "./SideBar";
+
+
+export {
+Navbar,
+Feed,
+VideoDetail,
+ChannelDetail,
+SearchFeed,
+SearchBar,
+SideBar
+}
