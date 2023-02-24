6ba80550b5195a07d6a71156e40059275e51fdb9
gitferreira <iniferre96@gmail.com>
2023-02-24 12:20:23 +0100

    wi[

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,12 +5,12 @@ import {VideoCard, ChannelCard } from './index'
 const Videos = ({videos}) => {
    
   return (
-   <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+   <Stack direction="row" flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
     {videos.map((item, idx) => {
         return (
         <Box key={idx}>
             {item.id.videoId && <VideoCard video = {item} />}
-            {item.id.channelId && <ChannelCard channelDetails = {item} />}
+            {item.id.channelId && <ChannelCard channelDetail = {item} />}
         </Box>
     )
     })}
